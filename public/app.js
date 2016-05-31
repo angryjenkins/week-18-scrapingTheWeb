663f2666b02a75e1e55be770a6ce7f3a3dc25fc7
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-31 17:08:26 -0400

    look into image links - some links are just loinks to imgur ofr flickr and don't go directly to pictures. Perhaps I will exclude these with a match in the model.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 $.getJSON('/articles', function(data) {
   for (var i = 0; i<data.length; i++){
-    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '</p>');
+    $('#articles').append('<div class="divider"></div>');
   }
 });
 
@@ -17,10 +18,11 @@ $(document).on('click', 'p', function(){
     .done(function( data ) {
       console.log(data);
       $('#notes').append('<p class="small">' + data.title + '</p>');
-      $('#notes').append('<div class="divider">');
+      $('#notes').append('<img class="materialboxed" width="100%" src="' +  data.link + '"/>');
 
       $('#notes').append('<input id="titleinput" name="title" >');
       $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
+      $('#notes').append('')
       $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
 
       if(data.note){
